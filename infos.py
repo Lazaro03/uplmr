9571ebb5cb94c196356bf964f0f7994023df2697
Jefferson BM <95538769+jefferson2001@users.noreply.github.com>
2023-02-17 11:33:17 -0500

    Update infos.py

--- infos.py
+++ infos.py
@@ -83,8 +83,8 @@ def createFinishUploading(filename,filesize,split_size,current,count,findex):
     msg = '✅Finalizado✅\n\n'
     msg+= '🧩Nombre: ' + str(filename)+'\n'
     msg+= '🗂Tamaño Total: ' + str(sizeof_fmt(filesize))+'\n'
-    msg+= '🗃Tamaño Partes:: ' + str(sizeof_fmt(split_size))+'\n'
     msg+= '📚Partes Subidas: ' + str(current) + '/' + str(count) +'\n\n'
+    msg+= '🗃Ver tu Archivo aqui: https://eduvirtual.uho.edu.cu/admin/tool/lp/user_evidence_list.php (tenga en cuenta los otros susuarios y no elimine nada que no sea de usted\n'
     msg+= '🌵Borrar Archivo: ' + '/del_'+str(findex)
     return msg
 
